cd512040ec602c5e551be1bb37b190d7a7fd92b1
OreoluwaElizabeth <oreoluwaadeyemo77@gmail.com>
2025-01-17 14:28:38 -0800

    Few styling here and there sign up and login working

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import Features from './Component/Features';
 import Register from './Component/Register';
 import Login from './Component/Login';
 import StudentDashboard from './Component/StudentDashboard';
+import Profile from './Component/Profile';
+import TeachersDashboard from './Pages/TeachersDashbard';
+import CreateCurriculum from './Components/CreateCurriculum';
+import CurriculumPage from './Pages/CurriculumPage';
+import LessonPlan from './Components/LessonPlan';
+import AvailableCurriculum from './Components/AvailableCurriculum';
+
 
 function App() {
     return (
@@ -13,13 +20,17 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/" element={<LandingPage />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/features" element={<Features />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />}/>
                 <Route path="/student/dashboard" element={<StudentDashboard />}/>
-
+                <Route path="/student/profile" element={<Profile />} />
+                <Route path="/teacher/dashboard" element={<TeachersDashboard />} />
+                <Route path="/create-curriculum" element={<CreateCurriculum />} />
+                <Route path="/curriculumPage" element={<CurriculumPage />} />
+                <Route path='/lessonPlan' element={<LessonPlan />} />
+                <Route path='AvaialableCurriculum' element={<AvailableCurriculum />} />
             </Routes>
         </BrowserRouter>
     );
